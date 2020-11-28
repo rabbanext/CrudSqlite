594dcc2a9e749b0e04b94638c795b03e906bc74b
R Muhammad Rabbani P <rabbaniarvian@gmail.com>
2020-11-28 17:38:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/darsiwan/crudsqliite/DataHelper.java
@@ -0,0 +1,37 @@
+package com.example.darsiwan.crudsqliite;
+
+
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "biodatadiri.db";
+    private static final int DATABASE_VERSION = 1;
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        String sql = "create table biodata(no integer primary key, nama text null, tgl text null, jk text null, alamat text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+        sql = "INSERT INTO biodata (no, nama, tgl, jk, alamat) VALUES ('1', 'Darsiwan', '1996-07-12', 'Laki-laki','Indramayu');";
+        db.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
